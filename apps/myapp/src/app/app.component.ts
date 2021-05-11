7295d95593cc0d7e014671b49ceba80957307044
Michael Schilling <mschilling@move4mobile.com>
2021-05-11 21:49:48 +0200

    rename org into krew

--- apps/myapp/src/app/app.component.ts
+++ apps/myapp/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'myorg-root',
+  selector: 'krew-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
