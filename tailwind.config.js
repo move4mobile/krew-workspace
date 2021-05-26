96ec7d35beb82c2d5cc768621972d60447028fbc
aaltw <awesthuis@move4mobile.com>
2021-05-26 20:42:49 +0200

    feat(krew-ui-angular): :sparkles: added krew-ui app project based on angular and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { guessProductionMode } = require('@ngneat/tailwind');
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
+module.exports = {
+  prefix: '',
+  mode: 'jit',
+  purge: {
+    content: [
+      './apps/krew-ui-angular/**/*.{html,ts,css,scss,sass,less,styl}',
+      './libs/krew-angular/**/*.{html,ts,css,scss,sass,less,styl}',
+    ],
+  },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
