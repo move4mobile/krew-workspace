cfcd50414b26eb709ed8255a7535547b683706ba
aaltw <awesthuis@move4mobile.com>
2021-05-28 13:53:27 +0200

    build(krew-ui-angular): :hammer: tailwind config now handles watch / production mode correctly

--- apps/krew-ui-angular/src/app/app.component.ts
+++ apps/krew-ui-angular/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'krew-root',
   template: `
-    <h1 class="p-8 text-lg flex justify-center">Welkom in Krew - Angular</h1>
+    <h1 class="p-8 text-lg flex justify-center border border-gray-500 bg-gray-50">Welkom in Krew - Angular</h1>
     <router-outlet></router-outlet>
   `,
 })
