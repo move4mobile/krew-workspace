ed924e306764cf39f41ab7514ba10d860e7e2064
aaltw <awesthuis@move4mobile.com>
2021-05-28 14:06:06 +0200

    refactor(krew-ui-angular): some more readable production mode guessing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
-process.env.TAILWIND_MODE =
-  process.env.WEBPACK_DEV_SERVER === 'true' && process.argv.join(' ').indexOf('build') !== -1 ? 'build' : 'watch';
+function guessProductionMode() {
+  const argv = process.argv.join(' ').toLowerCase();
+  const isProdEnv = process.env.NODE_ENV === 'production';
+  return isProdEnv || [' build', ':build', 'ng b', '--prod'].some(command => argv.includes(command));
+}
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
 module.exports = {
   prefix: '',
