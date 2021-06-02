c0faeffd5cdf14612e4bf9d61dc052575898fd47
cassshh <casvd@hotmail.com>
2021-06-02 17:29:04 +0200

    feat(krew-ui-svelte): extract scss to separate file

--- apps/krew-ui-svelte/src/App.svelte
+++ apps/krew-ui-svelte/src/App.svelte
@@ -8,27 +8,4 @@
   <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style lang="scss">
-  $brand01: hotpink;
-
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: $brand01;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-
-</style>
+<style src="./app.scss" lang="scss"></style>
