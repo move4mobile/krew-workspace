5e037fb21c281437818f4de269d8c4704201ec97
Michael Schilling <mschilling@move4mobile.com>
2021-07-01 21:50:29 +0200

    chore: import sdk

--- apps/krew-ui-svelte/src/routes/About.svelte
+++ apps/krew-ui-svelte/src/routes/About.svelte
@@ -1,1 +1,27 @@
+<script>
+  import { onMount } from 'svelte';
+
+  // import { Sdk } from '@krew/client-sdk';
+
+  // const ApiClient = new Sdk({
+  //   apiKey: process.env.TOKEN,
+  //   basePath: 'https://company-app.develop.m4mapp.com/api/',
+  // });
+
+  // create a prop
+  export let newsItem;
+
+  onMount(async () => {
+    await fetch(`https://jsonplaceholder.typicode.com/todos/1`)
+      // await ApiClient.getNewsItem('3a9d8d02-f34e-488e-8f10-aecbc41076d0')
+      .then(r => r.json())
+      .then(data => {
+        console.log(data);
+        newsItem = data;
+      });
+  });
+</script>
+
 <h3>About page!!!</h3>
+
+<h3>{newsItem?.title}</h3>
