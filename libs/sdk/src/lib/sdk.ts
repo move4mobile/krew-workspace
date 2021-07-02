061e5413bcb596edd9ef704e7beed0005721a0a4
Michael Schilling <mschilling@move4mobile.com>
2021-07-02 15:51:47 +0200

    chore: add resource link + repo reff

--- libs/sdk/src/lib/sdk.ts
+++ libs/sdk/src/lib/sdk.ts
@@ -1,3 +1,13 @@
+/**
+ *
+ * Inspired by:
+ *
+ * https://lyamkin.com/blog/how-to-build-api-client-library-in-js/
+ *
+ * https://github.com/ilyamkin/dev-to-js
+ *
+ */
+
 import { News } from './news';
 import { applyMixins } from './utils';
 import { Base } from './base';
