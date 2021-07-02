10d3f01ad1008ffb2a7a6dd72babe833229ee9d0
Michael Schilling <mschilling@move4mobile.com>
2021-07-02 10:00:01 +0200

    chore: initial progress

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '<rootDir>/libs/krew-angular/feature-shell',
     '<rootDir>/apps/krew-ui-react',
     '<rootDir>/libs/shared/assets',
+    '<rootDir>/libs/shared/environments',
   ],
 };
