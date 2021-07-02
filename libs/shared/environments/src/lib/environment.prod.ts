4a80a8f403dc8fc5f0dd6beb676e73409f75d7b7
Michael Schilling <mschilling@move4mobile.com>
2021-07-02 10:49:17 +0200

    chore: provide apiUrl in config file

--- libs/shared/environments/src/lib/environment.prod.ts
+++ libs/shared/environments/src/lib/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://company-app.develop.m4mapp.com/api',
+  apiClientId: '<to_be_provided>',
 };
--- libs/shared/environments/src/lib/environment.ts
+++ libs/shared/environments/src/lib/environment.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: false,
+  apiUrl: '<to_be_provided>',
+  apiClientId: '<to_be_provided>',
 };
