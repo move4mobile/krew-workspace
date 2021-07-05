45152da718c8cad1835d8410ebb2fce86ba0296c
aaltw <awesthuis@move4mobile.com>
2021-07-05 09:00:13 +0200

    feat(krew-ui-angular): :construction: added feature shell with DI

---
+++ libs/krew-angular/feature-shell/src/lib/shell.component.ts
@@ -0,0 +1,8 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  template: ` <krew-bottom-bar></krew-bottom-bar> `,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ShellComponent {}
