dbba21ca0ae8c15718e12bc27344fc9449d84718
Michael Schilling <mschilling@move4mobile.com>
2021-07-08 22:37:44 +0200

    chore: progress on mappers

--- libs/api-client/src/lib/services/news.service.ts
+++ libs/api-client/src/lib/services/news.service.ts
@@ -7,12 +7,21 @@ const resourceName = 'albums';
 
 export class News extends Base {
   async findOne(id: string): Promise<NewsItem> {
-    return this.request<NewsItem>(`${resourceName}/${id}`);
+    return this.request<NewsItem>(`${resourceName}/${id}`).then(serialize);
   }
 
   async findMany(): Promise<NewsItem[]> {
     let query = `${resourceName}`;
     query += '?limit=5&offset=0';
-    return this.request<NewsItem[]>(query);
+
+    return this.request<NewsItem[]>(query).then(serializeMany);
   }
+}
+
+function serialize(data: any): NewsItem {
+  return plainToClass(NewsItem, data);
+}
+
+function serializeMany(data: any[]): NewsItem[] {
+  return data.map(serialize);
 }
