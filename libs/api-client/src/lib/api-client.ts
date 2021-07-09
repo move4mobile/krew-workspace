0b580322e39af713187269b6e8dfae2579800a20
Michael Schilling <mschilling@move4mobile.com>
2021-07-09 21:30:19 +0200

    chore: extend auth service

--- libs/api-client/src/lib/api-client.ts
+++ libs/api-client/src/lib/api-client.ts
@@ -1,12 +1,16 @@
-export * from './core';
 import { Config } from './core';
 import { NewsService, AgendaService } from './services';
+import { AuthService } from './services/auth.service';
 
+export * from './core';
 export default function createKrewClient(config: Config) {
+  const _authService = new AuthService(config);
   const _newsService = new NewsService(config);
   const _agendaService = new AgendaService(config);
 
   return {
+    auth: () => _authService,
+
     news: () => _newsService,
 
     agenda: () => _agendaService,
--- libs/api-client/src/lib/services/base.service.ts
+++ libs/api-client/src/lib/services/base.service.ts
@@ -4,18 +4,6 @@ import * as fetchImport from 'isomorphic-unfetch';
 import { Config } from '../core';
 const fetch = (fetchImport.default || fetchImport) as typeof fetchImport.default;
 
-export type Params = {
-  [key: string]: any;
-};
-
-export interface IResourceGet {
-  get(id: string | number): Promise<any>;
-}
-
-export interface IResourceAll {
-  all(params?: Params): Promise<any[]>;
-}
-
 export abstract class BaseService {
   #clientId: string;
   #clientSecret: string;
@@ -23,8 +11,9 @@ export abstract class BaseService {
   #accessToken: string;
 
   constructor(config: Config) {
-    this.#clientId = config.clientId;
-    this.#clientSecret = config.clientSecret;
+    this.#clientId = config.clientId || '3c798074ceb26d43da9f0d6e845d3e44';
+    //prettier-ignore
+    this.#clientSecret = config.clientSecret || '928198f6183b96323d5572c93420cc2b922144aa72bb3974f98539a44cde840b10579f9d6031a38637b6e1b7119c1259b2b658b106a1553bac317e91edd435dc';
     this.#basePath = config.basePath || 'https://jsonplaceholder.typicode.com/';
   }
 
