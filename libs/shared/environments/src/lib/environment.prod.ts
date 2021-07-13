dcc548cb97b6b92098fc7c24b80f6dd535f1c318
Michael Schilling <mschilling@move4mobile.com>
2021-07-13 22:09:51 +0200

    chore: extend configurations

--- libs/shared/environments/src/lib/environment.prod.ts
+++ libs/shared/environments/src/lib/environment.prod.ts
@@ -1,5 +1,8 @@
-export const environment = {
+import { IEnvironment } from './environment.interface';
+
+export const environment: IEnvironment = {
   production: true,
-  apiUrl: '<to_be_provided>',
-  apiClientId: '<to_be_provided>',
+  apiUrl: 'api.companyapp.m4m.io',
+  apiClientId: '',
+  apiClientSecret: '',
 };
--- libs/shared/environments/src/lib/environment.ts
+++ libs/shared/environments/src/lib/environment.ts
@@ -1,5 +1,8 @@
-export const environment = {
+import { IEnvironment } from './environment.interface';
+
+export const environment: IEnvironment = {
   production: false,
   apiUrl: 'https://company-app.develop.m4mapp.com/api',
-  apiClientId: '<to_be_provided>',
+  apiClientId: '',
+  apiClientSecret: '',
 };
