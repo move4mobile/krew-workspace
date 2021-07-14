813db99872d50385aab3a1c6c80a4758161e36ba
Michael Schilling <mschilling@move4mobile.com>
2021-07-14 08:43:11 +0200

    chore: update apiUrl

--- libs/shared/environments/src/lib/environment.prod.ts
+++ libs/shared/environments/src/lib/environment.prod.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
   production: true,
-  apiUrl: 'api.companyapp.m4m.io',
+  apiUrl: 'https://api.companyapp.m4m.io/api',
   apiClientId: '',
   apiClientSecret: '',
 };
