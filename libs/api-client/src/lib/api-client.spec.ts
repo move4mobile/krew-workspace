abe02b92ce5d6dd7199adefdd1bbff028d0d125f
Michael Schilling <mschilling@move4mobile.com>
2021-07-21 13:14:13 +0200

    chore: update test spec

--- libs/api-client/src/lib/api-client.spec.ts
+++ libs/api-client/src/lib/api-client.spec.ts
@@ -2,6 +2,12 @@ import { createKrewClient } from './api-client';
 
 describe('apiClient', () => {
   it('should work', () => {
-    expect(createKrewClient({ apiKey: '' })).toBeDefined();
+    expect(createKrewClient({ sandbox: true })).toBeDefined();
+  });
+
+  it('should be able to fail login', async () => {
+    const apiClient = createKrewClient({ sandbox: true });
+
+    expect(await apiClient.auth().login('demo@move4mobile.com', '<enter password>')).toThrowError('Unauthorized');
   });
 });
