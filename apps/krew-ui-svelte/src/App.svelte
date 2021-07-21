b28ed1faeec150cd0b4ce43c5f26b19b0661a53d
cassshh <casvd@hotmail.com>
2021-07-21 20:36:45 +0200

    chore: fetch data

--- apps/krew-ui-svelte/src/App.svelte
+++ apps/krew-ui-svelte/src/App.svelte
@@ -1,11 +1,30 @@
 <script lang="ts">
-  import { Router, Route, Link } from 'svelte-routing';
+  import { createKrewClient, NewsItem } from '@krew/api-client';
+  import { onMount } from 'svelte';
+  import { Link, Route, Router } from 'svelte-routing';
   import About from './routes/About.svelte';
 
   export let name: string;
 
-  export let url = '';
+  let url = '';
+  let loading = true;
 
+  let newsItems: NewsItem[];
+
+  onMount(() => {
+    const client = createKrewClient({ sandbox: true });
+    client
+      .auth()
+      .login('', '')
+      .then(auth => {
+        loading = false;
+        console.log(auth);
+        client
+          .news()
+          .all()
+          .then(news => (newsItems = news));
+      });
+  });
 </script>
 
 <main>
@@ -20,6 +39,13 @@
     <Route path="/"><p>Hi there!</p></Route>
     <Route path="about" component={About} />
   </Router>
+
+  {#if loading}
+    <p>Loading...</p>
+  {:else}
+    <p>Loaded!</p>
+    {JSON.stringify(newsItems)}
+  {/if}
 </main>
 
 <style src="./app.scss" lang="scss"></style>
