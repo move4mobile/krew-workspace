19bac12d4c44ce89d92173fcff1f537f4185bc7d
Michael Schilling <mschilling@move4mobile.com>
2021-07-21 19:27:20 +0200

    chore: add lib luxon for time manip

--- libs/api-client/src/lib/services/agenda.service.ts
+++ libs/api-client/src/lib/services/agenda.service.ts
@@ -1,5 +1,6 @@
 import { AgendaItem, IResourceAll, Params } from '../core';
 import { BaseService } from './base.service';
+import { DateTime } from 'luxon';
 
 const resourceName = '/agenda';
 
@@ -7,25 +8,24 @@ export class AgendaService extends BaseService implements IResourceAll {
   async all(params?: Params): Promise<AgendaItem[]> {
     let query = `${resourceName}`;
 
-    const now = new Date();
-    let from = now.setDate(now.getDate() -30);
-    let to = now;
+    const now = DateTime.now();
+    let from = now.minus({ days: 30 }).toJSDate();
+    let to = now.toJSDate();
 
-    if (params.from){
-      from = params.from;
+    if (params.from) {
+      from = new Date(params.from);
     }
-    if (params.to){
-      to = params.to;
+    if (params.to) {
+      to = new Date(params.to);
     }
 
     const config = {
-      from: new Intl.DateTimeFormat('en-us').format(from),
-      to: new Intl.DateTimeFormat('en-us').format(to),
-    }
+      from: from.toISOString(),
+      to: to.toISOString(),
+    };
 
+    //prettier-ignore
     query += "?" + Object.keys(config).map(key => key + '=' + params[key]).join('&');
-
-    console.log(query);
 
     return this.request<AgendaItem[]>(AgendaItem, query);
   }
--- libs/api-client/src/lib/services/base.service.ts
+++ libs/api-client/src/lib/services/base.service.ts
@@ -42,6 +42,8 @@ export abstract class BaseService {
       headers,
     };
 
+    console.log('fetch ' + url);
+
     return fetch(url, config).then(r => {
       if (r.ok) {
         return plainToClass<T, any>(Model, r.json());
