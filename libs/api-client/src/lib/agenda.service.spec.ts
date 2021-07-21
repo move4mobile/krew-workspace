22844475f3373df7f3c5fa7c01428f2750779e30
Bas de Groot <bdegroot@move4mobile.com>
2021-07-21 18:26:06 +0200

    implemented news/agenda api

--- libs/api-client/src/lib/agenda.service.spec.ts
+++ libs/api-client/src/lib/agenda.service.spec.ts
@@ -1,18 +1,17 @@
 import { createKrewClient, NewsItemStatus } from './api-client';
 
 describe('apiClient', () => {
-  it('should be able to retrieve news', async () => {
+  it('should be able to retrieve agenda items', async () => {
     const apiClient = createKrewClient({ sandbox: true });
 
     // login
     await apiClient.auth().login(process.env.LOGIN_USER, process.env.LOGIN_PWD);
 
-    const news = await apiClient.news().all({
-      status: NewsItemStatus.PUBLISHED,
-      offset: 0,
-      limit: 10
+    const agendaItems = await apiClient.agenda().all({
+      from: new Date("2020-06-01"),
+      to: new Date("2020-07-01")
     });
 
-    expect(news.length).toBeGreaterThan(0);
+    expect(agendaItems.length).toBeGreaterThan(0);
   });
 });
--- libs/api-client/src/lib/services/agenda.service.ts
+++ libs/api-client/src/lib/services/agenda.service.ts
@@ -5,7 +5,27 @@ const resourceName = '/agenda';
 
 export class AgendaService extends BaseService implements IResourceAll {
   async all(params?: Params): Promise<AgendaItem[]> {
-    const query = `${resourceName}`;
+    let query = `${resourceName}`;
+
+    const now = new Date();
+    let from = now.setDate(now.getDate() -30);
+    let to = now;
+
+    if (params.from){
+      from = params.from;
+    }
+    if (params.to){
+      to = params.to;
+    }
+
+    const config = {
+      from: new Intl.DateTimeFormat('en-us').format(from),
+      to: new Intl.DateTimeFormat('en-us').format(to),
+    }
+
+    query += "?" + Object.keys(config).map(key => key + '=' + params[key]).join('&');
+
+    console.log(query);
 
     return this.request<AgendaItem[]>(AgendaItem, query);
   }
