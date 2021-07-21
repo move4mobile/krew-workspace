2554541753455d138a6679f595895df20de32e17
Bas de Groot <bdegroot@move4mobile.com>
2021-07-21 20:53:45 +0200

    Update own password api implementation

---
+++ libs/api-client/src/lib/services/password.service.ts
@@ -0,0 +1,36 @@
+import { BaseService } from './base.service';
+import { NewsItem } from '@krew/api-client';
+
+const resourceName = '/me/password';
+
+export class PasswordService extends BaseService {
+
+  /**
+   * Update the current password of the authenticated user.
+   *
+   * @param currentPassword
+   * @param newPassword
+   *
+   * @throws {@link CurrentPasswordIsInvalidError}
+   * This exception is thrown when the current password is invalid.
+   */
+  async updateOwnPassword(currentPassword: string, newPassword: string) {
+    const query = `${resourceName}`;
+
+    const postData = {
+      password: currentPassword,
+      new_password: newPassword
+    };
+
+    try {
+      this.post(query, postData);
+    }
+    catch (error){
+      if (error.message === "Invalid current password"){
+        throw new CurrentPasswordIsInvalidError();
+      }
+      throw error;
+    }
+  }
+
+}
