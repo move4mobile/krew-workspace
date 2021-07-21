61e6847e0a3848535759e897bc0d1c5ef9b43d64
Michael <mschilling@users.noreply.github.com>
2021-07-21 20:43:34 +0200

    feat(api-client): extend AgendaItem model (#238)

---
+++ libs/api-client/src/lib/core/enums/agenda-category.ts
@@ -0,0 +1,7 @@
+export enum AgendaCategory {
+  BIRTHDAY = 'birthday',
+  ANNIVERSARY = 'anniversary',
+  ACTIVITY = 'activity',
+  INFORMATION_EVENING = 'information_evening',
+  OTHER = 'other',
+}
--- libs/api-client/src/lib/core/models/agenda-item.model.ts
+++ libs/api-client/src/lib/core/models/agenda-item.model.ts
@@ -1,5 +1,13 @@
+import { AgendaCategory } from '../../core';
+
 export class AgendaItem {
   id: string;
   title: string;
-  date: string;
+  description: string;
+  location: string;
+  startTime: string;
+  endTime: string;
+  category: AgendaCategory;
+  employeesOnly: boolean;
+  allDay: boolean;
 }
