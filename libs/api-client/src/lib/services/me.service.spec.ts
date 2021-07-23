01ebbd83040be52431a0f97b89f982f03318f8a3
Michael Schilling <mschilling@move4mobile.com>
2021-07-23 10:05:29 +0200

    chore: improve me.service.spec testing

--- libs/api-client/src/lib/services/me.service.spec.ts
+++ libs/api-client/src/lib/services/me.service.spec.ts
@@ -1,15 +1,24 @@
 import { createKrewClient } from '../api-client';
 
+let apiClient;
+
+beforeEach(async () => {
+  apiClient = createKrewClient({ sandbox: true });
+
+  // login
+  await apiClient.auth().login(process.env.LOGIN_USER, process.env.LOGIN_PWD);
+});
+
 describe('meService', () => {
   it('should be able to retrieve me', async () => {
-    const apiClient = createKrewClient({ sandbox: true });
-
-    // login
-    await apiClient.auth().login(process.env.LOGIN_USER, process.env.LOGIN_PWD);
-
     const me = await apiClient.me().get();
-    console.log(me);
 
-    expect(me.id).toBeDefined();
+    expect(me).toHaveProperty('id');
+    expect(me).toHaveProperty('email');
+    expect(me).toHaveProperty('firstName');
+    expect(me).toHaveProperty('lastName');
+
+    expect(me['first_name']).toBeUndefined();
+    expect(me['last_name']).toBeUndefined();
   });
 });
