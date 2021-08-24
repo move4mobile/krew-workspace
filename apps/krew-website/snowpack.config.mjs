e3a6d7089b37e73e0903b34122af1b666cc427a4
Michael Schilling <mschilling@move4mobile.com>
2021-08-24 20:09:39 +0200

    build: add initial Astro app

---
+++ apps/krew-website/snowpack.config.mjs
@@ -0,0 +1,9 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+export default {
+  alias: {
+    $styles: './src/public/style',
+    $utils: './src/utils',
+  },
+};
