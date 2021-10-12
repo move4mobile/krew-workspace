f211f03e379b622f19cba83776071639a8bb0e66
Michael <mschilling@users.noreply.github.com>
2021-10-12 20:40:29 +0200

    feat: add GraphQL API to support crm data retrieval (#470)

---
+++ apps/krew-crm-api/src/employee-badges/employee-badges.resolver.ts
@@ -0,0 +1,33 @@
+import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { BadgesService } from '../../src/badges/badge.service';
+import { Badge } from '../../src/badges/models/badge.model';
+import { EmployeesService } from '../../src/employees/employees.service';
+import { Employee } from '../../src/employees/models/employee.model';
+import { EmployeeBadgesService } from './employee-badges.service';
+import { EmployeeBadge } from './models/employee-badge.model';
+
+@Resolver((of) => EmployeeBadge)
+export class EmployeeBadgesResolver {
+  constructor(
+    private readonly employeeBadgesService: EmployeeBadgesService,
+    private readonly employeesService: EmployeesService,
+    private readonly badgesService: BadgesService,
+  ) {}
+
+  @Query((returns) => [EmployeeBadge])
+  employeeBadges(): Promise<EmployeeBadge[]> {
+    return this.employeeBadgesService.findAll();
+  }
+
+  @ResolveField('employee', () => Employee)
+  getProject(@Parent() employeeBadge: EmployeeBadge) {
+    const { employeeId } = employeeBadge;
+    return this.employeesService.findOneById(employeeId + '');
+  }
+
+  @ResolveField('badge', () => Badge)
+  getBadge(@Parent() employeeBadge: EmployeeBadge) {
+    const { badgeId } = employeeBadge;
+    return this.badgesService.findOneById(badgeId + '');
+  }
+}
