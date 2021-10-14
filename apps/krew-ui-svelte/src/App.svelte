ec5cd3af30fa6b3055f4b9ebf0d30bf8203732ec
Michael Schilling <mschilling@move4mobile.com>
2021-10-14 22:13:05 +0200

    chore: add news page

--- apps/krew-ui-svelte/src/App.svelte
+++ apps/krew-ui-svelte/src/App.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   import { Router, Route, Link } from 'svelte-routing';
+  import News from './routes/News.svelte';
   import About from './routes/About.svelte';
 
   export let name: string;
 
   export let url = '';
-
 </script>
 
 <main>
@@ -14,10 +14,12 @@
   <Router {url}>
     <nav>
       <Link to="/">Home</Link>
+      <Link to="news">News</Link>
       <Link to="about">About</Link>
     </nav>
 
     <Route path="/"><p>Hi there!</p></Route>
+    <Route path="news" component={News} />
     <Route path="about" component={About} />
   </Router>
 </main>
---
+++ apps/krew-ui-svelte/src/core/environment.ts
@@ -0,0 +1,6 @@
+// Helper class
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://127.0.0.1:8022',
+};
