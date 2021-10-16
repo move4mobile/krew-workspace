7e29c24e62ebef405c14ff179ef6d6a53b8a0d40
Michael Schilling <mschilling@move4mobile.com>
2021-10-16 15:48:00 +0200

    chore: add members property to projects resolver

--- apps/krew-crm-api/src/projects/projects.module.ts
+++ apps/krew-crm-api/src/projects/projects.module.ts
@@ -2,11 +2,20 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../../src/database/database.module';
 import { DatabaseService } from '../../src/database/database.service';
 import { DateScalar } from '../common/scalars/date.scalar';
+import { EmployeeProjectsService } from '../employee-projects/employee-projects.service';
+import { EmployeesService } from '../employees/employees.service';
 import { ProjectsResolver } from './projects.resolver';
 import { ProjectsService } from './projects.service';
 
 @Module({
   imports: [DatabaseModule],
-  providers: [ProjectsResolver, ProjectsService, DatabaseService, DateScalar],
+  providers: [
+    ProjectsResolver,
+    ProjectsService,
+    EmployeesService,
+    EmployeeProjectsService,
+    DatabaseService,
+    DateScalar,
+  ],
 })
 export class ProjectsModule {}
--- apps/krew-crm-api/src/projects/projects.resolver.ts
+++ apps/krew-crm-api/src/projects/projects.resolver.ts
@@ -1,11 +1,18 @@
 import { NotFoundException } from '@nestjs/common';
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Parent, Args, Query, ResolveField, Resolver } from '@nestjs/graphql';
 import { ProjectsService } from './projects.service';
 import { Project } from './models/project.model';
+import { Employee } from '../employees/models/employee.model';
+import { EmployeeProjectsService } from '../employee-projects/employee-projects.service';
+import { EmployeesService } from '../employees/employees.service';
 
 @Resolver(() => Project)
 export class ProjectsResolver {
-  constructor(private readonly projectsService: ProjectsService) {}
+  constructor(
+    private readonly projectsService: ProjectsService,
+    private readonly employeesService: EmployeesService,
+    private readonly employeeProjectsService: EmployeeProjectsService,
+  ) {}
 
   @Query(() => Project)
   async project(@Args('id') id: string): Promise<Project> {
@@ -19,5 +26,17 @@ export class ProjectsResolver {
   @Query(() => [Project])
   projects(): Promise<Project[]> {
     return this.projectsService.findAll();
+  }
+
+  @ResolveField('members', () => [Employee])
+  async getMembers(@Parent() project: Project) {
+    const { id } = project;
+
+    const employeeProjects = await this.employeeProjectsService.findAll();
+    const employeeIds = employeeProjects.filter((e) => e.projectId + '' === id).map((ep) => ep.employeeId);
+
+    const employees = await this.employeesService.findAll();
+
+    return employees.filter((e) => employeeIds.includes(e.id));
   }
 }
