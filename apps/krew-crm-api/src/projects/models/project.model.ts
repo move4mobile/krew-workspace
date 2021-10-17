5da95592c7d4ab52e8aef9debe9fb557e49e1c7b
Michael Schilling <mschilling@move4mobile.com>
2021-10-17 15:12:04 +0200

    chore: add JiraProjectStatus enum

--- apps/krew-crm-api/src/projects/models/project.model.ts
+++ apps/krew-crm-api/src/projects/models/project.model.ts
@@ -1,9 +1,12 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { JiraProjectStatus } from '../../common/enums/jira-project-status.enum';
 import { parseNumber } from '../../common/utils/sheets-parser.utils';
 
 enum FieldMapping {
   ID = 'ID',
   NAME = 'Name',
+  DESCRIPTION = 'Description',
+  STATUS = 'Status',
   JIRA_KEY = 'JiraKey',
 }
 
@@ -16,13 +19,21 @@ export class Project {
   name: string;
 
   @Field({ nullable: true })
+  description: string;
+
+  @Field({ nullable: true })
   jiraKey: string;
+
+  @Field(() => JiraProjectStatus, { nullable: true })
+  status: JiraProjectStatus;
 
   static fromRow(data: any) {
     const obj = Object.assign(new Project(), <Partial<Project>>{
       id: parseNumber(data[FieldMapping.ID]),
       name: data[FieldMapping.NAME],
+      description: data[FieldMapping.DESCRIPTION],
       jiraKey: data[FieldMapping.JIRA_KEY],
+      status: data[FieldMapping.STATUS] ? (data[FieldMapping.STATUS] as JiraProjectStatus) : undefined,
     });
 
     return obj;
