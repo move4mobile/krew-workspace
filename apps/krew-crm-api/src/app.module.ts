07014ff340b248cce29d53f4834c7c4ca6b307e4
Michael Schilling <mschilling@move4mobile.com>
2021-10-19 08:17:27 +0200

    chore: remove unused imports

--- apps/krew-crm-api/src/app.module.ts
+++ apps/krew-crm-api/src/app.module.ts
@@ -5,13 +5,9 @@ import { BadgesModule } from './badges/badges.module';
 import { DatabaseModule } from './database/database.module';
 import { DatabaseService } from './database/database.service';
 import { EmployeeBadgesModule } from './employee-badges/employee-badges.module';
-import { EmployeeBadgesService } from './employee-badges/employee-badges.service';
 import { EmployeeProjectsModule } from './employee-projects/employee-projects.module';
-import { EmployeeProjectsService } from './employee-projects/employee-projects.service';
 import { EmployeesModule } from './employees/employees.module';
-import { EmployeesService } from './employees/employees.service';
 import { ProjectsModule } from './projects/projects.module';
-import { ProjectsService } from './projects/projects.service';
 
 @Module({
   imports: [
