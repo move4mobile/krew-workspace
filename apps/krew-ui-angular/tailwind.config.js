efe3b6d7204318094e9dba2757110ae3e136c3e7
Aalt Westhuis <35026618+aaltw@users.noreply.github.com>
2021-11-26 14:36:28 +0100

    feat: update tailwind usage in krew-angular (issue #608) (#609)
    * chore(krew-ui-angular): :truck: move tailwind.config.js to krew-ui-angular folder
    
    * refactor(krew-ui-angular): stripping down tailwind.config
    
    * chore(krew-angular-lib): clean up login component
    
    * chore(krew-angular-lib): more console.log cleanup
    
    * build(krew-ui-angular): :arrow_up: update with npm install (also updates package-lock.json)
    
    * feat(krew-ui-angular): added example for defining css vars differently and it's usage
    
    * refactor(krew-ui-angular): making a factory method

--- apps/krew-ui-angular/tailwind.config.js
+++ apps/krew-ui-angular/tailwind.config.js
@@ -1,18 +1,23 @@
+/**
+ * @param {string} varName
+ * @returns method used in tailwind config for creating custom colors with opacity
+ */
+const withOpacity =
+  varName =>
+  ({ opacityValue }) =>
+    `rgba(var(${varName}), ${opacityValue || 1})`;
+
 module.exports = {
-  prefix: '',
   mode: 'jit',
   purge: {
-    enabled: process.env.TAILWIND_MODE === 'build',
-    content: [
-      './apps/krew-ui-angular/**/*.{html,ts,css,scss,sass,less,styl}',
-      './libs/krew-angular/**/*.{html,ts,css,scss,sass,less,styl}',
-    ],
+    enabled: true,
+    content: ['apps/krew-ui-angular/**/*.{html,ts}', 'libs/krew-angular/**/*.{html,ts}'],
   },
   darkMode: 'media',
   theme: {
     extend: {
       colors: {
-        'brand-01': 'var(--brand-01)',
+        'brand-01': withOpacity('--brand-01'),
         'brand-01-darker': 'var(--brand-01-darker)',
         'brand-01-lighter': 'var(--brand-01-lighter)',
 
