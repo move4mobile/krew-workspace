b2155b276811f350c091948faa317861aa5f0267
Michael Schilling <mschilling@move4mobile.com>
2021-12-03 20:08:04 +0000

    chore: fix jest in krew-angular-ui

--- apps/krew-ui-angular/src/app/app.component.ts
+++ apps/krew-ui-angular/src/app/app.component.ts
@@ -4,4 +4,6 @@ import { Component } from '@angular/core';
   selector: 'krew-root',
   template: ` <router-outlet></router-outlet> `,
 })
-export class AppComponent {}
+export class AppComponent {
+  title = 'krew-ui-angular';
+}
