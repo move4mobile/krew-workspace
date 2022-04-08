4bc0591a9078c64498220affc9aa3d50fbe1f770
Michael Schilling <mschilling@move4mobile.com>
2022-04-08 09:57:31 +0200

    fix: update astro config file to non-legacy

--- apps/krew-website/astro.config.mjs
+++ apps/krew-website/astro.config.mjs
@@ -1,19 +1,10 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from 'astro/config';
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  // projectRoot: '.', // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // src: './src',
-  dist: '../../dist/apps/krew-website',
-  public: './public', // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+// https://astro.build/config
+export default defineConfig({
+  outDir: '../../dist/apps/krew-website',
   integrations: [],
+  publicDir: './public', // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   vite: {
     plugins: [],
     builds: {
