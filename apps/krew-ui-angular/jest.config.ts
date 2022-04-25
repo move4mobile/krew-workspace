732e0f55f10188a0485e94a645ae7c9aa1ab4fca
Michael <mschilling@users.noreply.github.com>
2022-04-25 15:46:26 +0200

    build(deps): migratie nx to 14.x (#1008)
    * migrate nx to 14.x
    
    * update dependabot config

--- apps/krew-ui-angular/jest.config.ts
+++ apps/krew-ui-angular/jest.config.ts
@@ -1,23 +1,22 @@
 module.exports = {
-  displayName: 'shared-environments',
-  preset: '../../../jest.preset.js',
+  displayName: 'krew-ui-angular',
+
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: {
-        before: [
-          'jest-preset-angular/build/InlineFilesTransformer',
-          'jest-preset-angular/build/StripStylesTransformer',
-        ],
-      },
     },
   },
-  coverageDirectory: '../../../coverage/libs/shared/environments',
+  coverageDirectory: '../../coverage/apps/krew-ui-angular',
+  transform: {
+    '^.+.(ts|mjs|js|html)$': 'jest-preset-angular',
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
+  preset: '../../jest.preset.ts',
 };
--- apps/krew-ui-react/jest.config.ts
+++ apps/krew-ui-react/jest.config.ts
@@ -1,10 +1,11 @@
 module.exports = {
   displayName: 'krew-ui-react',
-  preset: '../../jest.preset.js',
+
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/krew-ui-react',
+  preset: '../../jest.preset.ts',
 };
