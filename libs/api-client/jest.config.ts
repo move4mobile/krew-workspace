7aa91da06d44920008e4536f915c03b8e38f8845
Michael Schilling <mschilling@move4mobile.com>
2022-06-03 09:02:55 +0200

    chore: migrate nx

--- libs/api-client/jest.config.ts
+++ libs/api-client/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'api-client',
 
   globals: {
@@ -11,5 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/api-client',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
 };
--- libs/krew-angular/feature-shell/jest.config.ts
+++ libs/krew-angular/feature-shell/jest.config.ts
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   displayName: 'krew-angular-feature-shell',
 
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
@@ -20,5 +20,5 @@ module.exports = {
     'jest-preset-angular/build/serializers/ng-snapshot',
     'jest-preset-angular/build/serializers/html-comment',
   ],
-  preset: '../../../jest.preset.ts',
+  preset: '../../../jest.preset.js',
 };
