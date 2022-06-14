86e979fc80be6fa81961dc5d1b84de5c64037749
nratering <89972776+nratering@users.noreply.github.com>
2022-06-14 10:58:33 +0200

    fix: tailwind fixed tailwind and updated astro to version 1 beta

--- apps/krew-website/astro.config.mjs
+++ apps/krew-website/astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/krew-website',
-  integrations: [],
+  integrations: [tailwind()],
   publicDir: './public', // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
   vite: {
     plugins: [],
